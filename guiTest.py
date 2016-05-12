e705578eb179cf5eae79ce32b966c36e07886ec5
Nidoprince <Nidoprince@gmail.com>
2016-05-13 00:16:39 +0300

    More tests on how to use git

--- guiTest.py
+++ guiTest.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import random
 from placeMaker import *
+from haiku import haiku
 
 class Application(tk.Frame):
     def __init__(self, master=None):
---
+++ haiku.py
@@ -0,0 +1,2 @@
+def haiku(suggestion):
+    return "This Haiku I did\nWrite late at night and upload\nas a test of code"
